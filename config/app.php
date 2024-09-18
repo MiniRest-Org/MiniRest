c9ad44634dc769c2ff44193df2720decb53fbbce
Marcone Santos <98716688+Marcone-Santos1@users.noreply.github.com>
2024-09-18 20:24:06 -0300

    update miniRest framework version

--- config/app.php
+++ config/app.php
@@ -1,7 +1,19 @@
 <?php
 
-
 return [
+    'name' => 'My Application',
+    'env' => 'production',
+    'debug' => false,
     'root_path' => dirname(__DIR__, 1),
     'views_path' => dirname(__DIR__, 1) . '/resources/views/',
+
+    'providers' => [
+        \MiniRestFramework\Providers\RouterServiceProvider::class,
+        \MiniRestFramework\Providers\RouteServiceProvider::class,
+        \MiniRestFramework\Providers\TemplateEngineServiceProvider::class
+    ],
+
+    'aliases' => \MiniRestFramework\Support\Facades\Facade::defaultAliases()->merge([
+
+    ])
 ];
